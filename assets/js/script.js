1d5eb01fbdf36e9f0537e5b8be4554c76b87a0d2
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2021-12-29 11:16:17 -0600

    fixed sybtax error in array

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,16 +33,16 @@ var minimumSpecialCharacters = "";
 
 //Random character array//
 var inputArray = {
-  getNumbers: function() {
+  getNumbers: function () {
     return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
   },
-  getUpperCases: function() {
+  getUpperCases: function () {
     return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
   },
-  getLowerCases: function() {
-    return String.fromCharCode(Math.floor(Math.random * 26 + 65));
+  getLowerCases: function () {
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
   },
-  getSpecialCharacters: function() {
+  getSpecialCharacters: function () {
     return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
   }
 };
